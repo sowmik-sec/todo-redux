78e87ceec972b7b8743df84050dccc24683dcdde
sowmik-sec <sowmik.sec@gmail.com>
2024-12-13 21:46:43 +0600

    project initialization

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h2 className="text-3xl">My Todo</h2>
+      <Button>My Button</Button>
+    </>
+  );
+}
+
+export default App;
