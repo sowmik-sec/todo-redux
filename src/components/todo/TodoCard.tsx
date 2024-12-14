b9a06a3a22d035bfa68b7df074d0898dfba0af9c
sowmik-sec <sowmik.sec@gmail.com>
2024-12-14 20:33:37 +0600

    Toggle Complete state

--- src/components/todo/TodoCard.tsx
+++ src/components/todo/TodoCard.tsx
@@ -1,20 +1,39 @@
 import { useAppDispatch } from "@/redux/hook";
 import { Button } from "../ui/button";
-import { removeTodo } from "@/redux/features/tosoSlice";
+import { removeTodo, toggleComplete } from "@/redux/features/tosoSlice";
 
 type TTodoCardProps = {
   id: string;
   title: string;
   description: string;
+  isCompleted?: boolean;
 };
 
-const TodoCard = ({ id, title, description }: TTodoCardProps) => {
+const TodoCard = ({ id, title, description, isCompleted }: TTodoCardProps) => {
   const dispatch = useAppDispatch();
+
+  const toggleState = () => {
+    console.log("clicked");
+    dispatch(toggleComplete(id));
+  };
+
   return (
     <div className="bg-white rounded-md flex justify-between items-center p-3 border">
-      <input type="checkbox" name="" id="" />
+      <input
+        onChange={toggleState}
+        type="checkbox"
+        name="complete"
+        id="complete"
+      />
       <p className="font-semibold">{title}</p>
-      <p>Time</p>
+      {/* <p>Time</p> */}
+      <div>
+        {isCompleted ? (
+          <p className="text-green-500">Done</p>
+        ) : (
+          <p className="text-yellow-500">Pending</p>
+        )}
+      </div>
       <p>{description}</p>
       <div className="space-x-5">
         <Button onClick={() => dispatch(removeTodo(id))} className="bg-red-500">
--- src/components/todo/TodoContainer.tsx
+++ src/components/todo/TodoContainer.tsx
@@ -12,14 +12,18 @@ const TodoContainer = () => {
         <TodoFilter />
       </div>
       <div className="bg-red-500 bg-primary-gradient w-full h-full p-[5px] rounded-xl ">
-        <div className="bg-white p-5 w-full h-full rounded-lg space-y-3">
-          {todos.map((item) => (
-            <TodoCard {...item} />
-          ))}
-        </div>
-        {/* <div className="bg-white p-5 text-2xl font-bold flex justify-center items-center rounded-md">
-          <p>There is no task pending</p>
-        </div> */}
+        {todos.length && (
+          <div className="bg-white p-5 w-full h-full rounded-lg space-y-3">
+            {todos.map((item) => (
+              <TodoCard {...item} />
+            ))}
+          </div>
+        )}
+        {!todos.length && (
+          <div className="bg-white p-5 text-2xl font-bold flex justify-center items-center rounded-md">
+            <p>There is no task pending</p>
+          </div>
+        )}
       </div>
     </div>
   );
