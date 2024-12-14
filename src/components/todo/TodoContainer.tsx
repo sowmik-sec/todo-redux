c4690ef035d39866c42dbcec8c40c6d3e41f5dbb
sowmik-sec <sowmik.sec@gmail.com>
2024-12-14 21:00:41 +0600

    use Query in RTK Query

--- src/components/todo/TodoContainer.tsx
+++ src/components/todo/TodoContainer.tsx
@@ -1,10 +1,19 @@
-import { useAppSelector } from "@/redux/hook";
+// import { useAppSelector } from "@/redux/hook";
 import AddTodoModal from "./AddTodoModal";
 import TodoCard from "./TodoCard";
 import TodoFilter from "./TodoFilter";
+import { useGetTodosQuery } from "@/redux/api/api";
 
 const TodoContainer = () => {
-  const { todos } = useAppSelector((state) => state.todos);
+  // from local state
+  // const { todos } = useAppSelector((state) => state.todos);
+
+  const { data: todos, isLoading, isError } = useGetTodosQuery(undefined);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
   return (
     <div>
       <div className="flex justify-between mb-5">
@@ -14,7 +23,7 @@ const TodoContainer = () => {
       <div className="bg-red-500 bg-primary-gradient w-full h-full p-[5px] rounded-xl ">
         {todos.length && (
           <div className="bg-white p-5 w-full h-full rounded-lg space-y-3">
-            {todos.map((item) => (
+            {todos?.data.map((item) => (
               <TodoCard {...item} />
             ))}
           </div>
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import todoReducer from "./features/tosoSlice";
+import { baseApi } from "./api/api";
 
 export const store = configureStore({
   reducer: {
+    [baseApi.reducerPath]: baseApi.reducer,
     todos: todoReducer,
   },
+  middleware: (getDefaultMiddlewares) =>
+    getDefaultMiddlewares().concat(baseApi.middleware),
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
